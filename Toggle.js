a71f8debed8921913536fa830399eaf10d449c2f
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2021-12-22 10:03:14 +0200

    fixed linters errors

--- Toggle.js
+++ Toggle.js
@@ -1,15 +1,14 @@
-
 const hamburger = document.querySelector('#hamburger');
 const closetog = document.querySelector('#togclose');
 const toggleMenu = document.querySelector('.toggle-menu');
 
 function show() {
-    toggleMenu.style.display = 'flex';
-    toggleMenu.style.top = '0';
+  toggleMenu.style.display = 'flex';
+  toggleMenu.style.top = '0';
 }
 
 function close() {
-    toggleMenu.style.top = '-100%';
+  toggleMenu.style.top = '-100%';
 }
 
 hamburger.addEventListener('click', show);
