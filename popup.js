d793d68d689cc1c70873a037483f6e47a8135b9f
abdulrahmanshr75 <94563329+abdulrahmanshr75@users.noreply.github.com>
2021-12-24 10:40:07 +0200

    add the description

--- popup.js
+++ popup.js
@@ -78,6 +78,8 @@ const projects = [
         <li class="languagesdef">Terminal</li>
         <li class="languagesdef">Codepen</li>
         </ul>
+        <p class = "pop-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea</p>
+        </div>
         `
     };
 
